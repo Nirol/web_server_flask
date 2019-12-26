e95abbdff825463f19bfd5de423727b35576d2e0
Bukagg <nirolgg@gmail.com>
2019-12-26 10:41:26 +0200

    fixed bugs, running now

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,8 +1,20 @@
-
-from app.models import User
+from app import create_app, db
+from app.models import User, Role
 import unittest
 
 class UserModelTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        Role.insert_roles()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
 
 
     def test_password_setter(self):
